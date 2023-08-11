4c8a52b70f6927c6afcd3e4ede713830c94f11bd
ellencheampong8 <acheampongellen649@gmail.com>
2023-08-10 17:34:34 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /** 
  * main - this c file program uses puts instead of printf
- * returns 0
+ * return : 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
