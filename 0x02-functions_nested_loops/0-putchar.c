7a7eb29ebc0d19242db3d12aaff534315fb5b9e5
ellencheampong8 <acheampongellen649@gmail.com>
2023-08-15 16:15:59 -0700

    so many eerors

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,26 @@
 #include "main.h"
+
+
 /**
- * main - print _putchar
- * is written by mbah
- * Return: 0
- */
+ * main - Prints _putchar
+ * void: Empty parameter.
+ *
+ * Description: It prints _putchar
+ *
+ * Return: zero on success
+ **/
 
 int main(void)
 {
-putchar('');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+       	_putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+        return (0);
 }
 
