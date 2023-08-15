149bd628542d9f5c25bfae9bdf62e963cec55363
ellencheampong8 <acheampongellen649@gmail.com>
2023-08-15 16:12:30 -0700

    pushed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,21 @@
 #include "main.h"
 /**
- * main - Prints _putchar
- * void: Empty parameter.
- *
- * Description: It prints _putchar
- *
- * Return: zero on success
-*/
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
 
 int main(void)
 {
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
-        return (0);
+putchar('');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
 
