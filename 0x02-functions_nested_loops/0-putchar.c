9298f89d4dfe4122e2630ef29d9f65cad1e5f347
ellencheampong8 <acheampongellen649@gmail.com>
2023-08-15 15:54:02 -0700

    real corrctions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,20 @@
  *
  * Description: It prints _putchar
  *
- * Return: zero on successs
+ * Return: zero on success
 */
 
 int main(void)
 {
         _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+        return (0);
 }
 
