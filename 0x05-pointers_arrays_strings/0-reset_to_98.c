e8ae2f705c53d2c7898097cbec74097e971e2df4
ellencheampong8 <acheampongellen649@gmail.com>
2023-08-21 12:20:15 -0700

    98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,10 @@
 /**
  * reset_to_98 - updates the value
  * @n: integer to be checked
- * created by Ellen 
+ * created by Ellen
+ * Return: 0 on success 
  */
+
 void reset_to_98(int *n)
 {
 *n = 98;
