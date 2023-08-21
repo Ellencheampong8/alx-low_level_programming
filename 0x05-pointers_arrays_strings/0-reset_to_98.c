ca5f3b2f165c221eaccbd641f8719800183183e4
ellencheampong8 <acheampongellen649@gmail.com>
2023-08-21 12:27:28 -0700

    98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * reset_to_98 - updates the value
- * @n: int to be checked
- * created by Ellen
- * Return: 0 on success 
- */
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  Eng Ellen
+*  Return: 0 is success
+*/
 
 void reset_to_98(int *n)
 {
-*n = 98;
+	*n = 98;
 }
 
