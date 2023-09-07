fd973307db2e46a59e71a91c4b32b5c13acba4c3
ellencheampong8 <acheampongellen649@gmail.com>
2023-09-07 09:26:16 -0700

    more mallocs

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * malloc_checked - it causes normal process termination
+ * @b: allocated memory
+ * Return: returns a pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *i;
+
+	i = malloc(b);
+	if (i == NULL)
+		exit(98);
+	return (i);
+
+}
